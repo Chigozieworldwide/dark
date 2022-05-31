dad20ad131bdede344ca63ada535729ed8d2ca5f
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-31 19:11:03 +0100

    .

--- cython.py
+++ cython.py
@@ -6,8 +6,8 @@ try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("cython").mulai()
+	__import__("darkk").mulai()
 elif 'aarch' in arc:
-	__import__("cythonn").mulai()
+	__import__("dark").mulai()
 else:
 	exit(f' Unknow device machine {arc}')
